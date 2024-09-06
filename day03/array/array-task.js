94d106be144c5cbc43c0a254289c69e0ee1f899a
dev-cdy <argon4067@gmail.com>
2024-09-06 17:48:43 +0900

    day04

--- day03/array/array-task.js
+++ day03/array/array-task.js
@@ -14,9 +14,49 @@ console.log(arr3.map(c => hangles.charAt(c)).join(""))
 console.log(arr3.map(data => hangles[data]).join(""))
 // 4) 1~100까지 합 출력
 const arr4 = new Array(100).fill(0)
+let sum = 0;
+(arr4.map((data,i) => i+1).forEach((data) => sum += data));
+console.log(sum)
 // console.log(arr4.map((data,i) => i+1)).reduce(function(result){
 // })
 // 5) 문자열 반대로 출력하기 .reverse() 사용
+  console.log(hangles.split("").reverse().join(""))
 // 6) 전화번호를 파라미터로 받아서 파싱하기(데이터를 바꿔주기)
+const phoneNum = "010-1234-5678"
+// console.log(phoneNum.replaceAll("1234","****"))
+
+const phoneNumber = "010-1234-5678".split("");
+phoneNumber.splice(4,4,"*", "*","*","*").join("")
+console.log(phoneNumber.join(""));
+
 // ex) 010-1234-5678
 // result) 010-****-5678
+
+const phoneNumber1 = "010-1234-5678".split("-");
+phoneNumber1[1] ="****";
+const parsePhone = phoneNumber1.join("-")
+console.log(parsePhone)
+
+
+///////////////////////////////////////////////////////////
+// 4) 1~100까지 합 출력
+let result = 0;
+new Array(100).fill(0).map((data, i) => i + 1).forEach(data => { result += data })
+console.log(result)
+
+// 5) 문자열 반대로 출력하기 .reverse() 사용
+let result2 = "apple".split("").reverse().join("");
+console.log(result2)
+
+// 6) 전화번호를 파라미터로 받아서 파싱하기
+// ex) 010-1234-5678
+// result) 010-****-5678
+
+// const phoneNumber = "010-1234-5678".split("-");
+// phoneNumber[1] = "****";
+// const parsePhoneNumber = phoneNumber.join("-")
+// console.log(parsePhoneNumber)
+
+// const phoneNumber2 = "010-1234-5678".split("-");
+// const parsePhoneNumber2 = phoneNumber2.splice(1,1,"****")
+// console.log(phoneNumber2.join("-"));
--- day03/array/array.js
+++ day03/array/array.js
@@ -10,74 +10,74 @@
 // console.log(arr4)
 
 // .split(), String 안에서 사용
-// const phoneNumber = "010-9999-5555";
+const phoneNumber = "010-9999-5555";
+console.log(phoneNumber.split("-"))
 
 // // .join(), array 안에서 사용
-// console.log(phoneNumber.split("-"))
-// console.log(phoneNumber.split("-").join(""))
+console.log(phoneNumber.split("-").join(""))
 
 // // .replace(), String 안에서 사용
 // // .replaceAll(), String 안에서 사용
-// console.log(phoneNumber.replaceAll("9","*"))
+console.log(phoneNumber.replaceAll("9","*"))
 
 // // 추가
 // // .push(), array 안에서 사용
 // // 기존의 배열의 마지막 위치에 값을 추가한다.
-// const arr =["김세환", "홍길동", "장보고"]
-// arr.push("마피아", "흰둥이", "대마왕")
-// console.log(arr)
+const arr =["김세환", "홍길동", "장보고"]
+arr.push("마피아", "흰둥이", "대마왕")
+console.log(arr)
 
 // // .concat(), array
 // // 두 개의 배열을 [새로운 배열]로 리턴한다.
-// const arr1 = ["짱구", "고길동"]
-// const arr2 = ["철수","영회"]
-// const arr3 = arr1.concat(arr2);
+const arr1 = ["짱구", "고길동"]
+const arr2 = ["철수","영회"]
+const arr3 = arr1.concat(arr2);
 // //배열끼리 더하면 문자열이 된다
-// console.log(arr1+arr2)
+console.log(arr1+arr2)
 // // 두 개의 배열을 더해서 *새로운* 배열 생성
-// console.log(arr1.concat(arr2));
+console.log(arr1.concat(arr2));
 
 // 제거 / 유사 잘라내기
 // .pop(), array
 // 마지막 요소를 제거하고 그 값을 반환한다.
-// const arr4 = [10,20,30,40];
-// console.log(arr4.pop())
-// console.log(arr4)
+const arr4 = [10,20,30,40];
+console.log(arr4.pop())
+console.log(arr4)
 
 // // .unshift()
 // // 맨 앞에 요소를 추가하는 메서드
-// const arr5 = [10,20,30,40];
-// arr5.unshift(200)
-// console.log(arr5)
+const arr5 = [10,20,30,40];
+arr5.unshift(200)
+console.log(arr5)
 
 // // shift()
 // // 맨 앞에 요소를 제거하는 메서드
-// console.log(arr5.shift())
-// console.log(arr5)
+console.log(arr5.shift())
+console.log(arr5)
 
 // .slice(start, end) array
 // startIndex부터 endIndex 전까지 요소를 1depth 복사하여 새로운 배열을 반환한다.<안 짤라간다>
-// const arr6 =[10,20,30,40,50,60,70,80,90,100];
+const arr6 =[10,20,30,40,50,60,70,80,90,100];
 
 // //
-// console.log(arr6.slice(4,8))
-// console.log(arr6)
-// console.log(arr6.slice(-4,-1))
+console.log(arr6.slice(4,8))
+console.log(arr6)
+console.log(arr6.slice(-4,-1))
 // // 매개변수를 1개만 전달하면 끝까지!
-// console.log(arr6.slice(4))
-// console.log(arr6.slice(-4))
+console.log(arr6.slice(4))
+console.log(arr6.slice(-4))
 
 // .splice(startIndex, deleteCount) String
 // 제거 및 수정
-// const arr7 =[10,20,30,40,50,60,70,80,90,100];
-//  console.log(arr7.splice(2,3))
+const arr7 =[10,20,30,40,50,60,70,80,90,100];
+ console.log(arr7.splice(2,3))
 // // 수정
-// // .splice(startIndex, deleteCount)
-// console.log(arr7.splice(3,1,400))
+// .splice(startIndex, deleteCount)
+console.log(arr7.splice(3,1,400))
 //  // .splice(startIndex, deleteCount, ...pushItems)
 //  // 그 자리에 아이템만큼 넣기
-//  console.log(arr7.splice(3,1,400,400,400,400,400))
-//  console.log(arr7)
+ console.log(arr7.splice(3,1,400,400,400,400,400))
+ console.log(arr7)
 
 // 정렬
 // .sort() array
